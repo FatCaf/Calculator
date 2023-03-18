6106ba4182d91d1c8800c665af2a78b1190036bb
FatCaf <124441574+FatCaf@users.noreply.github.com>
2023-03-18 22:32:56 +0200

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -21,8 +21,58 @@ function backspace() {
 
 function calculateResult() {
   let calculation = result.innerHTML;
+  let operator = '';
+  let operands = [];
+
+  if (calculation.indexOf("%") !== -1) {
+    operands = calculation.split('%');
+    operator = '%';
+  } else if (calculation.indexOf("+") !== -1) {
+    operands = calculation.split('+');
+    operator = '+';
+  } else if (calculation.indexOf("-") !== -1) {
+    operands = calculation.split('-');
+    operator = '-';
+  } else if (calculation.indexOf("*") !== -1) {
+    operands = calculation.split('*');
+    operator = '*';
+  } else {
+    result.innerHTML = calculation;
+    return;
+  }
+
+  const num1 = parseFloat(operands[0]);
+  const num2 = parseFloat(operands[1]);
+
+  let answer;
+
+  switch (operator) {
+    case '+':
+      answer = num1 + num2;
+      break;
+    case '-':
+      answer = num1 - num2;
+      break;
+    case '*':
+      answer = num1 * num2;
+      break;
+    case '%':
+      answer = num1 / 100 * num2;
+      break;
+  }
+
+  result.innerHTML = answer;
+}
+
+
+
+
+/*
+function calculateResult() {
+  let calculation = result.innerHTML;
   if (calculation.indexOf("%") !== -1) {
     calculation = calculation.replace(/%/g, "/100");
   }
   result.innerHTML = eval(calculation);
 }
+*/
